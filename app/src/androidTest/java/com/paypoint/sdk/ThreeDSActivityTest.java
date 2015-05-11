c8823536b35b58c293af38f2e592eeac51b3a1ae
Pete Hendry <peterhendry@paypoint.com>
2015-05-11 11:47:36 +0100

    Added 3DSActivity tests

--- app/src/androidTest/java/com/paypoint/sdk/ThreeDSActivityTest.java
+++ app/src/androidTest/java/com/paypoint/sdk/ThreeDSActivityTest.java
@@ -4,8 +4,13 @@
 
 package com.paypoint.sdk;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.InstrumentationTestCase;
 
@@ -13,9 +18,16 @@ import com.paypoint.sdk.library.ThreeDSActivity;
 
 import junit.framework.Assert;
 
+import org.hamcrest.MatcherAssert;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+import static com.jayway.awaitility.Awaitility.*;
+import static com.jayway.awaitility.Awaitility.await;
 
 /**
  * Who:  Pete
@@ -25,6 +37,8 @@ import org.junit.Test;
 public class ThreeDSActivityTest extends ActivityInstrumentationTestCase2<ThreeDSActivity> {
 
     private ThreeDSActivity activity;
+    private boolean responseReceived;
+    private boolean success;
 
     public ThreeDSActivityTest() {
         super(ThreeDSActivity.class);
@@ -35,21 +49,66 @@ public class ThreeDSActivityTest extends ActivityInstrumentationTestCase2<ThreeD
         super.setUp();
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
 
-        Intent intent = new Intent(getActivity(), ThreeDSActivity.class);
-        intent.putExtra(ThreeDSActivity.EXTRA_ACS_URL, "http:\\www.bbc.co.uk");
-//        intent.putExtra(ThreeDSActivity.EXTRA_TERM_URL, threeDSecure.getTermUrl());
-//        intent.putExtra(ThreeDSActivity.EXTRA_PAREQ, threeDSecure.getPareq());
-//        intent.putExtra(ThreeDSActivity.EXTRA_MD, threeDSecure.getMd());
-//        intent.putExtra(ThreeDSActivity.EXTRA_TRANSACTION_ID, paymentResponse.getTransactionId());
-//        intent.putExtra(ThreeDSActivity.EXTRA_SESSION_TIMEOUT, threeDSecure.getSessionTimeout());
-//        intent.putExtra(ThreeDSActivity.EXTRA_ALLOW_SELF_SIGNED_CERTS, isCustomUrl);
+        // this sets up a fast redirect where the ACS page returns the term URL after a few
+        // seconds without any user input
+        Intent intent = new Intent(getInstrumentation().getContext(), ThreeDSActivity.class);
+        intent.putExtra(ThreeDSActivity.EXTRA_ACS_URL, "http://192.168.3.242:5000/acs/auth");
+        intent.putExtra(ThreeDSActivity.EXTRA_TERM_URL, "http://192.168.3.242:5000/landingpage/");
+        intent.putExtra(ThreeDSActivity.EXTRA_PAREQ, "VALID_PAREQ_FAST_REDIR");
+        intent.putExtra(ThreeDSActivity.EXTRA_MD, "7897162297");
+        intent.putExtra(ThreeDSActivity.EXTRA_TRANSACTION_ID, "7897162297");
+        intent.putExtra(ThreeDSActivity.EXTRA_SESSION_TIMEOUT, 3600000);
+        intent.putExtra(ThreeDSActivity.EXTRA_ALLOW_SELF_SIGNED_CERTS, true);
         setActivityIntent(intent);
-        activity = getActivity();
 
+        activity = getActivity();
     }
 
     @Test
-    public void test3DS() {
-        Assert.assertTrue(true);
+    public void test3DSSuccess() {
+
+        // register to receive event from 3DS activity
+        activity.registerReceiver(new ThreeDSecureReceiver(),
+                new IntentFilter(ThreeDSActivity.ACTION_COMPLETED));
+
+        // wait for 10 seconds for thread to become unblocked
+        try {
+            await().atMost(10, TimeUnit.SECONDS).until(responseReceived());
+        } catch (Throwable e) {
+            // swallow
+        }
+        Assert.assertTrue(success);
+    }
+
+    @Test
+    public void test3DSTimeout() {
+
+        // register to receive event from 3DS activity
+        activity.registerReceiver(new ThreeDSecureReceiver(),
+                new IntentFilter(ThreeDSActivity.ACTION_COMPLETED));
+
+        // wait for 1 second for thread to come unblocked
+        try {
+            await().atMost(1, TimeUnit.SECONDS).until(responseReceived());
+        } catch (Throwable e) {
+            // swallow
+        }
+        Assert.assertFalse(success);
+    }
+
+    private Callable<Boolean> responseReceived() {
+        return new Callable<Boolean>() {
+            public Boolean call() throws Exception {
+                return responseReceived; // The condition that must be fulfilled
+            }
+        };
+    }
+
+    private class ThreeDSecureReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            success = intent.getBooleanExtra(ThreeDSActivity.EXTRA_SUCCESS, false);
+            responseReceived = true;
+        }
     }
 }
